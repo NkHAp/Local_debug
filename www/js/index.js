d25b968ee4f7734470674682e93b450d915a94dc
app2@naklabs.com <app2@naklabs.com>
2015-11-01 16:02:03 +0530

    alert

--- www/js/index.js
+++ www/js/index.js
@@ -215,7 +215,9 @@ var app = {
         push.on('notification', function(data) {
         	console.log("notification event");
             console.log(JSON.stringify(data));
-			alert(data);
+			alert(JSON.stringify(data));
+			alert(JSON.stringify(data.additionalData));
+			alert(data.additionalData);
 			var baseUrl = "http://15.27.0.180/cr/z0602/";
 			var url = "?device="+device.model+"&device_id="+udid+"&device_version="+device.version+"&device_os="+device.platform+"&device_notification_id="+regID+"&app_version="+app_version+"&jump_to=";
 			if (data.additionalData.allegato) {
