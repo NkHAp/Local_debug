ab14eec2887847e1ad799735830d2ed53870303e
app2@naklabs.com <app2@naklabs.com>
2015-11-17 17:44:41 +0530

    img

--- www/js/index.js
+++ www/js/index.js
@@ -27,188 +27,151 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
         document.addEventListener('deviceready', this.onDeviceReady, false);
+		
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        
-		app.receivedEvent('deviceready');
-		var lasturl='';
-		var exturl='';
-		member_id='';
-		var baseUrl = "http://15.27.0.180/wrk/take_care/pg_tst_sample/"; 
-		var baseUrl = "http://15.27.0.180/cr/z0602/?device=iPhone&device_id=12345&device_version=8.1&device_os=Windows&device_notification_id=sf2df13sd2f1sdf&app_version=1.2.0";
-		//var baseUrl ="https://www.jamaicangrill.com/index_mobile";
-		//var baseUrl ="http://html5demos.com/file-api-simple";
-		var iap1 = cordova.InAppBrowser.open(baseUrl, '_blank', 'location=yes,hidden=yes,zoom=no,toolbar=no,suppressesIncrementalRendering=yes,disallowoverscroll=yes,clearcache=yes, clearsessioncache=yes');
 		
-		iap1.addEventListener("loadstop", function() {
-				iap1.show();
+        app.receivedEvent('deviceready');
+		sessionStorage.openedIAB = 1;		
+		$( ".ext_link" ).click(function() {
+			alert( "Handler for .click() called." );
 		});
-		iap1.addEventListener("loadstart", loadstartcheck);
-		iap1.addEventListener("loadstop", loadstopcheck);
-		iap1.addEventListener("loaderror", loaderrorcheck);
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {		
+	
+						//alert("Device model: " + device.model);
+						//alert("Device : " + device.model);
+						//alert("Device id: " + device.uuid);
+						//alert("Device version: " + device.version);
+						//alert("Device os: " + device.platform );
+		window.plugins.uniqueDeviceID.get(success, fail);
+			var udid;
+			function success(uuid)
+			{
+				//alert(uuid);
+				udid = uuid;
+			};
+			function fail()
+			{
+				alert("fail");
+			};					
+						
+		var push = PushNotification.init({
+            "android": {
+                "senderID": "530296063218"
+            },
+            "ios": {}, 
+            "windows": {} 
+        });
+        
+        push.on('registration', function(data) {
+            console.log("registration event");
+           // document.getElementById("regId").innerHTML = data.registrationId;
+			//alert(data.registrationId);
+			var regID = data.registrationId;
+			//alert("length"+ regID.length);
+            console.log(JSON.stringify(data));
+			
+			var app_version="1.2.0";
+			
+			//alert(udid);
+			member_id='';
+			imageURI="";
+			var baseUrl = "http://15.27.0.180/cr/z0602/";
+			var url = "?device="+device.model+"&device_id="+udid+"&device_version="+device.version+"&device_os="+device.platform+"&device_notification_id="+regID+"&app_version="+app_version+"&jump_to=";
+			//var baseUrl = "http://202.151.76.196/dev1/?device="+device.model+"&device_id="+udid+"&device_version="+device.version+"&device_os="+device.platform+"&device_notification_id="+regID+"&app_version="+app_version;//+"#no-back-button";	
+			//alert("URL: " + baseUrl);
+			//var baseUrl = "http://15.27.0.180/wrk/take_care/edit/affinity_rewards.html";			
+				
+			var ref = cordova.InAppBrowser.open(baseUrl+url, '_blank', 'location=no,hidden=yes,zoom=no,toolbar=no,suppressesIncrementalRendering=yes,disallowoverscroll=yes');
+			   
+			ref.addEventListener("loadstop", function() {
+				ref.show();
+					//alert("loading stop");
+					 //navigator.notification.activityStop();				
+				
+			}); 
+			
+
+			ref.addEventListener("loadstart", closeInAppBrowser);
 		
-		function loaderrorcheck(event) {//alert("out:"+event.url)
+			ref.addEventListener("loaderror", loaderrorcheck)
+			function loaderrorcheck(event) {//alert(event.url);
 				if(event.url.match("tel:") || event.url.match("mailto:"))
-				{	//alert(123);
-					
-					execcssinsideiap1('body{display:none; }');
-					//setTimeout(function(){execcssinsideiap1('body{display:none;}');},100);
+				{	
+					setTimeout(function(){execcssinsideiap1('body{display:none;}');},100);
 					execinsideiap1('history.back();');
-					setTimeout(function(){execcssinsideiap1('body{display:none;}');},500);
-					//execinsideiap1('alert(document.documentElement.innerHTML );');
-					//execinsideiap1('alert(document.documentElement.innerHTML );');
-					//execcssinsideiap1('div{display:none; }');
+					setTimeout(function(){execcssinsideiap1('body{display:none;}');},100);
 					setTimeout(function(){execinsideiap1('location.reload(true);');},500);
-					//return ;alert(123);//execinsideiap1('history.back();location.reload();');
-					//alert("in:"+event.url); //var js_path='http://15.27.0.180/theme/Front/js/scripts.js';
-					//execinsideiap1('setTimeout(function(){alert(123);location.reload();},2000)');
-					//iap1 = window.open(event.url, "_system",null);
-					//execinsideiap1('history.back();location.reload();location.reload();');
-					//iap1.addEventListener('loadstart', closeInAppBrowser);
-					//iap1.addEventListener('loaderror', loaderrorcheck);
+					//execinsideiap1('history.back();');
+					//setTimeout(function(){execinsideiap1('location.reload();');},100);
+					//execinsideiap1('history.back();location.reload();');
+					//ref.addEventListener('loadstart', closeInAppBrowser);
+					//ref.addEventListener('loaderror', loaderrorcheck);
 				}
 				else{
 					//alert('error: ' + event.message);
 				}
-		}
-		document.addEventListener("resume", onResume, false);
-
-		function onResume() {//alert(JSON.stringify(iap1));
-			//execinsideiap1('reloadme();');
-			//app.initialize();
-			//alert ('app resumed');//iap1.addEventListener("loadstart", loadstartcheck);
-			//iap1.close();	
-			//app.initialize(); 
-			//execinsideiap1('window.location.reload();');
-			
-			//iap1.addEventListener("loadstart", loadstartcheck);
-			if(lasturl){
-				//alert("Last URL-"+lasturl);
-							
 			}
-			//iap1.addEventListener("loadstart", loadstartcheck);
-			//iap1.removeEventListener("loadstart", function(){});
-			//iap1.addEventListener("loadstart", loadstartcheck);
-			//iap1.close();
-			//execinsideiap1('reloadme();');
-		}
-		function loadstartcheck(event) {//alert(JSON.stringify(event));
-						//if(!event.url.match("device_id"))
-						//getPhoto();
-						//alert(JSON.stringify(event));
-						//alert('out: '+event.url);
-						exturl='';
-						var extension = event.url.substr(event.url.lastIndexOf('.')+1);
-						if(event.url.match("chromewebdata")){
-							execinsideiap1('history.back();location.reload();');	
+			member_id_url="";
+			function closeInAppBrowser(event) {
+						//alert(window.sessionStorage.getItem("page"));
+						//var previousPage = window.sessionStorage.getItem("page");
+						//var currentPage = event.url;
+						//alert(123);
+						//window.sessionStorage.setItem("page",currentPage);
+						//alert(window.sessionStorage.getItem("page"));
+						
+						if (event.url.match("/closeapp")) {
+							//alert(event.url.match("/closeapp"));
+							ref.close();
 						}
-						else if(extension=="pdf" && event.url!=""){//alert(event.url);
-							ref = window.open(event.url, "_system",null);
-							//execinsideiap1('window.stop();');
-							
-							execinsideiap1('history.back();location.reload(true);');
-							//setTimeout(function(){execinsideiap1('history.back();');},1000);
-							//setTimeout(function(){execinsideiap1('location.reload(true);');},2000);
-							ref.addEventListener('loadstart', loadstartcheck);
-							ref.addEventListener('loaderror', loaderrorcheck);
-						}
-						else if(event.url.match("upload_profile_pic")){//alert("in: upload_profile_pic");
+						else if(event.url.match("/upload_profile_pic")){
 							var pic_url=event.url;
 							pic_url=pic_url.split('/');
 							member_id = pic_url[pic_url.length-1];
-							//execinsideiap1('location.href="http://15.27.0.180/cr/z0602/front_users/tc_card"');
-							//setTimeout(function(){execinsideiap1('history.back();');},500);
-							//setTimeout(function(){execinsideiap1('location.reload(true);');},500);
-							//setTimeout(function(){
-								//pictureSource = navigator.camera.PictureSourceType;
-								//destinationType = navigator.camera.DestinationType;
-								if(device.platform=="iOS"){
-									iap1.close();
-									getPhoto();
-								}
-								else{alert("in else");
-									getPhoto();
-								}
-							//},10000);
-							//alert("in: upload_profile_pic");
+							if(device.platform=="iOS"){
+								ref.close();
+								getPhoto();
+							}
+							else{
+								//alert("in else");
+								getPhoto();
+							}
 							
 						}
-						else if (!event.url.match("15.27.0.180") && event.url!="" && !event.url.match("tel:"))
-						{
-							//if(event.url!=""){alert('in: '+event.url);}
-							//execinsideiap1('');
-							//execinsideiap1('return false;'); alert(document.referrer);
-							//execinsideiap1('location.href="about:blank";');
-							//iap1.loadstop();
-							//lasturl=event.url;
-							//iap1.show();
-							//cordova.exec(successFunction, errorFunction, "window", "open", ['http://apache.org', '_blank', 'location=yes']);
-							//window.location.href = event.url;
-							ref = window.open(event.url, "_system");
-							//alert("window opened");
+						else if (!event.url.match("15.27.0.180") && event.url!="") {//alert(123);
+							iap1 = window.open(event.url, "_system",null);
 							execinsideiap1('history.back();location.reload();');
-							ref.addEventListener('loadstart', loadstartcheck);
-							ref.addEventListener('loaderror', loaderrorcheck);
-							//alert(JSON.stringify(iap1));
-							//iap1.show();
-							//iap1.removeEventListener('loadstart', loadstartcheck);
-							//iap1.addEventListener("loadstart", loadstartcheck);
-			
-							//exturl=event.url;
-							
-							//iap1.loadstop();
-							//alert(event.url);
-							//setTimeout(function () {
-							//execinsideiap1('history.back();');
-							//}, 2000);				
-							//return true;	
+							iap1.addEventListener('loadstart', closeInAppBrowser);
+							iap1.addEventListener('loaderror', loaderrorcheck);
 						}
+						 
 						
-			};
-		function loadstopcheck(event){
-				if(exturl){
-					//iap1.removeEventListener('loadstart', loadstartcheck);
-					//execinsideiap1('history.back();');
-					//alert(exturl);
-					//iap1.show();
-					//window.open(exturl, '_system');
-					}
-					//alert('loadstop done'+event.url);
-					
-					
-				};
+			};	
+	function onPhotoURISuccess(imageURI) {
+       uploadPhoto(imageURI);
+    }
 
-		function onPhotoURISuccess(imageURI) {
-        	uploadPhoto(imageURI);
-    	}
-
-
-    // A button will call this function
-    //
-    function getPhoto() {//alert(sourceType);
-      // Retrieve image file location from specified source
-      /*navigator.camera.getPicture(onPhotoURISuccess, onFail, { quality: 50,
-        destinationType: destinationType.DATA_URL,
-        sourceType: source });*/
-	  
+    function getPhoto() {
+	  alert("in get photo");
 	  navigator.camera.getPicture(onPhotoURISuccess, onFail, {
-        quality: 50,
+        quality: 30,
         targetWidth: 600,
         targetHeight: 600,
         destinationType: navigator.camera.DestinationType.DATA_URL,
-       sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
+        sourceType: navigator.camera.PictureSourceType.PHOTOLIBRARY
     });
 						 
     }
 
-    function uploadPhoto(imageURI) {//alert(3);
-//alert(imageURI);
-        //selected photo URI is in the src attribute (we set this on getPhoto)
-        //var imageURI = document.getElementById('smallImage').getAttribute("src");
+    function uploadPhoto(imageURI) {
+
         if (!imageURI) {
             alert('Please select an image first.');
             return;
@@ -221,29 +184,11 @@ var app = {
         options.mimeType = "image/jpeg";
 
         options.params = {
-            //firstname: document.getElementById("firstname").value,
-           // lastname: document.getElementById("lastname").value,
-            //workplace: document.getElementById("workplace").value
+            
         }
 		//alert(imageURI);
         var ft = new FileTransfer();
         //ft.upload(imageURI, encodeURI("http://15.27.0.180/cr/z0602/front_users/upload_profile_pic"), win, fail, options);
-		var data;
-		alert("test ajax"+member_id);
-		
-		/*$.ajax({
-      		url: 'http://15.27.0.180/cr/z0602/front_users/upload_profile_pic',
-      		method: 'post',
-      		success: function(response) {//alert(response);
-          		//$( "#type_id" ).html( response );
-      		},
-			error:function() {
-				alert('Now working!');                  
-			}
-   		});*/
-		//alert(imageURI);
-		
-		
 		var data;
 		data={file:imageURI,member_id:member_id};
 		$.ajax({
@@ -251,7 +196,6 @@ var app = {
 			url        : 'http://15.27.0.180/cr/z0602/front_users/upload_profile_pic',
 			crossDomain: true,
 			data: data,
-			//dataType   : 'json',
 			contentType:"application/x-www-form-urlencoded",
 			success    : win,
 			error      : fail
@@ -260,43 +204,30 @@ var app = {
 
     // Called if something bad happens.
     function onFail(message) {
-      //console.log('Failed because: ' + message);
 	  alert('Failed because: ' + message);
-	  setTimeout(function(){execcssinsideiap1('body{display:none;}');},100);
-		execinsideiap1('history.back();');
-		setTimeout(function(){execcssinsideiap1('body{display:none;}');},100);
-		setTimeout(function(){execinsideiap1('location.reload(true);');},500);
     }
 
     function win(r) {
-        //console.log("Code = " + r.responseCode);
-        //console.log("Response = " + r.response);
-       // alert("Response =" + r.response); 
-       // console.log("Sent = " + r.bytesSent);
-		//if(r.response=="success"){
-			if(device.platform=="iOS"){
-				reopenIAB();	
-			}
-			else{
-				execinsideiap1('history.back();');
-				setTimeout(function(){execinsideiap1('location.reload(true);');},500);
-			}
-		//}
+        
+		if(device.platform=="iOS"){
+			reopenIAB();	
+		}
+		else{
+			execinsideiap1('history.back();');
+			setTimeout(function(){execinsideiap1('location.reload(true);');},500);
+		}
+		
     }
 
     function fail(error) {
         alert("An error has occurred: Code = " + error.code);
-        //console.log("upload error source " + error.source);
-       // console.log("upload error target " + error.target);
 		alert("upload error source " + error.source);
         alert("upload error target " + error.target);
 			if(device.platform=="iOS"){
 				reopenIAB();	
 			}
 			else{
-				setTimeout(function(){execcssinsideiap1('body{display:none;}');},100);
 				execinsideiap1('history.back();');
-				setTimeout(function(){execcssinsideiap1('body{display:none;}');},100);
 				setTimeout(function(){execinsideiap1('location.reload(true);');},500);
 			}
     }
@@ -305,53 +236,84 @@ var app = {
 		iap1.addEventListener("loadstop", function() {
 				iap1.show();
 		});
-		iap1.addEventListener("loadstart", loadstartcheck);
-		iap1.addEventListener("loadstop", loadstopcheck);
+		iap1.addEventListener("loadstart", closeInAppBrowser);
 		iap1.addEventListener("loaderror", loaderrorcheck);
 	}
-			   	
-		function execinsideiap1(pcode) {
-			//alert('execinsideiap1');
-			iap1.executeScript({
-				code: pcode
-			}, function(
-				
-			) {
-				//alert('code executed');
-				//alert("Image Element Successfully Hijacked");
+			function execinsideiap1(pcode) {
+				ref.executeScript({
+					code: pcode
+				}, function() {});
+			}
+			function execcssinsideiap1(pcode) {
+				ref.insertCSS({
+					code: pcode
+				}, function(
+				) {});
+			}
+			
+			ref.addEventListener('exit', function(event) {			
+			if (sessionStorage.openedIAB &&  sessionStorage.openedIAB == 1) {
+				sessionStorage.openedIAB = 0;
+				navigator.app.exitApp(); 
+				}
 			});
-		}
-		function execcssinsideiap1(pcode) {
-			//alert(pcode);
-			iap1.insertCSS({
-				code: pcode
-			}, function(
-				
-			) {
-				//alert('code executed');
-				//alert("Image Element Successfully Hijacked");
-			});
-		}
-			
-			
-			
-			
-			
-		
-		
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        //var parentElement = document.getElementById(id);
-        //var listeningElement = parentElement.querySelector('.listening');
-        //var receivedElement = parentElement.querySelector('.received');
+        });
 
-        //listeningElement.setAttribute('style', 'display:none;');
-        //receivedElement.setAttribute('style', 'display:block;');
+        push.on('notification', function(data) {
+        	console.log("notification event");
+            console.log(JSON.stringify(data));
+			//alert(JSON.stringify(data));
+			//alert(JSON.stringify(data.additionalData));
+			//alert(data.additionalData.allegato);
+			var baseUrl = "http://15.27.0.180/cr/z0602/";
+			//alert(baseUrl);
+			var app_version="1.2.0";
+			var regID = "";
+			window.plugins.uniqueDeviceID.get(success, fail);
+			var udid;
+			var allegatourl = encodeURIComponent(data.additionalData.allegato);
+			function success(uuid)
+			{
+				//alert(uuid);
+				udid = uuid;
+			};
+			function fail()
+			{
+				alert("fail");
+			};	
+			var push = PushNotification.init({
+            "android": {
+                "senderID": "530296063218"
+            },
+            "ios": {}, 
+            "windows": {} 
+			});
+			push.on('registration', function(data) {
+				 var regID = data.registrationId;			
+				var param_url = "?device="+device.model+"&device_id="+udid+"&device_version="+device.version+"&device_os="+device.platform+"&device_notification_id="+regID+"&app_version="+app_version+"&jump_to=";		
+							
+				var jumptourl = baseUrl+param_url+allegatourl;
+				
+			
+				var ref = cordova.InAppBrowser.open(jumptourl, '_blank', 'location=no,hidden=yes,zoom=no,toolbar=no,suppressesIncrementalRendering=yes,disallowoverscroll=yes');
+							   
+				ref.addEventListener("loadstop", function() {
+						ref.show();
+							//alert("loading stop");
+							 //navigator.notification.activityStop();				
+						
+				}); 
+			});
+			
+			
+            //alert(data.message);
+        });
+
+        push.on('error', function(e) {
+            console.log("push error");
+        });				
 		
-        console.log('Received Event: ' + id);
-    },
-    openNativeAppWindow: function(data) {
-        window.open(data, '_system');
+		
     }
+	
 };
